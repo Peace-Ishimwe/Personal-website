8666925d102890f8c604e635b1c917955660e23b
Peace-Ishimwe <peaceishimwem@gmail.com>
2023-07-05 05:14:44 +0200

    propish

---
+++ int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n = 10;
+    int fib = fibonacci(n);
+    
+    printf("The 10th value of the Fibonacci series is: %d\n", fib);
+    
+    return 0;
+}
